0df250d05fb170b8c83a070e48e0529d03887261
Hnicoulin <82177528+Hnicoulin@users.noreply.github.com>
2021-04-19 23:12:28 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function (event) {
+
+    document.getElementById("button1").addEventListener("click", function () {
+
+        document.getElementById("words").value = "success";
+    });
+});
+
